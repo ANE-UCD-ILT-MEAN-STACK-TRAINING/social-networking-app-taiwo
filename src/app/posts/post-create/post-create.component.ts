0518e7c0e1c5df7d2781fa2e6490a68198f50316
Oyesanmi <taiwo.oyesanmi@accenture.com>
2020-05-05 23:01:23 -0400

    updates

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  enteredValue = '';
+
+  newPost = 'No Content';
+  
+  onAddPost(){   
+    this.newPost=this.enteredValue;
+  }
+
+
+}
