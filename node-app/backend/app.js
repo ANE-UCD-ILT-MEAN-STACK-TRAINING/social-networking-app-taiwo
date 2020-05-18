4e5a1adf60b35273c1b67b722da311bd3920fa3f
Oyesanmi <taiwo.oyesanmi@accenture.com>
2020-05-18 10:22:57 -0400

    updates

--- node-app/backend/app.js
+++ node-app/backend/app.js
@@ -1,13 +1,26 @@
+const path = require("path");
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-// Lets attach the body-parser middleware
-// bodyParser.json() -> this will tell only to process json type data from the request body
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb://localhost:27017/MyPosts?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 app.use(bodyParser.json());
-//another example showing body-parser can process other types of body other than json
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,44 +30,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-
-  // we still need to send the response as we dont want client be waiting and timeout
-  res.status(201).json({
-    message: "Post added successfully",
-  });
-});
-
-//app.get();
-//app.put();
-//app.delete();
-
-app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server",
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!",
-    },
-  ];
-  res.status(200).json({
-    message: "Posts fetched succesfully!",
-    posts: posts,
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
-
-
-
